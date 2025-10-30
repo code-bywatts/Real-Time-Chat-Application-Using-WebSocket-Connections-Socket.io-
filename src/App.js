621def3d28c6f7fd29bf547c15fc8eeef32512b8
Vasu Watts <vasuwattsuser@gmail.com>
2025-10-30 14:14:33 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Chat from "./Chat";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>💬 Real-Time Chat App</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
